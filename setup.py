4b80ec32d3ea46d642b2e86f69ed26589c5187c4
AmnadTaowsoam <amnadtaow@gmail.com>
2022-12-24 13:21:04 +0700

    add setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+import datetime
 from my_db.db import FPDB
+from my_db.dbb import FPDBB
 
 db = FPDB()
+dbb = FPDBB()
 
 if __name__=="__main__":
         try:
             db.create_fpanalysis_tbl()
+            dbb.create_fpbuffer_tbl()
+            print('create_master_tbl successfully','(',datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),')')
         except:
-            print('create_copellet_tbl error')
+            print('create_master_tbl error','(',datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),')')
